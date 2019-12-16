11c20d864c225bfe32ad759dc524872f947ba0aa
Bogdan Dascal <bogdan.dascal@alight.eu>
2019-12-16 15:01:53 +0200

    Make multiple selection possible; Make persons unique; Fix display issues

--- scripts/submitForm.js
+++ scripts/submitForm.js
@@ -2,24 +2,26 @@ let personsList = [];
 function onLoad(){
     if(localStorage.getItem('persons') !== null) {
         personsList = JSON.parse(localStorage.getItem('persons'));
-        refreshTable();
-    
+        refreshTable(personsList);
     }
+    if(localStorage.getItem("identity") == null)
+        localStorage.setItem("identity", JSON.stringify(0));
 }
 
 function handleSubmit() {
     validateForm();
     if(formIsValid())
     {
+        let id = JSON.parse(localStorage.getItem("identity")) + 1;
         let firstName = getInputValue('firstNameSection');
         let lastName = getInputValue('lastNameSection');
         let age = getInputValue('ageSection');
         let gender = getInputValue('genderSection')
         let employed = getInputValue('employmentSection')
         let study = getInputValue('studiesSection');
-        addPerson(firstName, lastName, age, gender, employed, study);
-        refreshTable(firstName, lastName, age, gender, employed, study);
-        clearForm();
+        addPerson(id, firstName, lastName, age, gender, employed, study);
+        refreshTable(personsList);
+        //clearForm();
     }
 }
 
@@ -61,55 +63,70 @@ function getInputValue(sectionId) {
 }
 
 function addPerson(...values) {
-    personsList.push({ 'firstName': values[0], 'lastName': values[1], 'age': values[2], 'gender': values[3], 'employed': values[4], 'study': values[5]});
+    personsList.push({ 'id': values[0], 'firstName': values[1], 'lastName': values[2], 'age': values[3], 'gender': values[4], 'employed': values[5], 'study': values[6]});
     updateLocalStorage();
 }
 
 function updateLocalStorage() {
     localStorage.setItem("persons", JSON.stringify(personsList));
+    var identity = JSON.parse(localStorage.getItem("identity"));
+    localStorage.setItem("identity", JSON.stringify(identity + 1));
 }
 
-function refreshTable() {
+function refreshTable(list) {
     var table = document.getElementById('personsTable');
     var rowCount = table.rows.length;
     for (var x=rowCount-1; x>0; x--) 
         table.deleteRow(x);
-    for(var j=0; j<personsList.length;j++){
-        insertRowInTable(table, j);
+    for(var j=0; j<list.length;j++){
+        insertRowInTable(table, j, list);
     }
     //mergeRows();
 }
 
-function insertRowInTable(table, j) {
+function insertRowInTable(table, j, list) {
     var newRow = table.insertRow();
     var rowIndex = j;
     let cells = [];
     for (var i = 0; i < table.rows[0].cells.length; i++)
         cells[i] = newRow.insertCell(i);
-    person = personsList[j];
-    cells[0].innerHTML = person['firstName'];
-    cells[1].innerHTML = person['lastName'];
-    cells[2].innerHTML = person['age'];
-    cells[3].innerHTML = person['gender'];
-    cells[4].innerHTML = person['employed'];
-    cells[5].innerHTML = person['study'];
-    insertButtonInRow(table, cells);
+    person = list[j];
+    cells[1].innerHTML = person['id'];
+    cells[2].innerHTML = person['firstName'];
+    cells[3].innerHTML = person['lastName'];
+    cells[4].innerHTML = person['age'];
+    cells[5].innerHTML = person['gender'];
+    cells[6].innerHTML = person['employed'];
+    cells[7].innerHTML = person['study'];
+    insertActionButtonsInRow(table, cells);
 }
 
-function insertButtonInRow(table, cells) {
+function insertActionButtonsInRow(table, cells) {
     var rowIndex = table.rows.length - 2;
-    cells[cells.length - 1].innerHTML = `<div class="fakeButton" onclick="deletePerson(${rowIndex})">Delete</div><div></div><div class="fakeButton" onclick="handleEdit(${rowIndex})">Edit</div>`;
+    var personId = table.rows[rowIndex+1].cells[1].innerHTML;
+    cells[0].innerHTML = `<input type="checkbox" class="tableCheckBox">`;
+
+    cells[cells.length - 1].innerHTML = `<div class="fakeButton" onclick="deletePerson(${personId})">Delete</div><div></div><div class="fakeButton" onclick="handleEdit(${personId})">Edit</div>`;
 }
 
-function deletePerson(rowIndex){
+function deletePerson(id){
     if(document.querySelector("#cancelButton").hidden == false) 
         alert("You are already in a process. Cancel it and try again");
     else {
-        personsList.splice(rowIndex, 1);
-        updateLocalStorage();
-        refreshTable();
-        alert("Delete was successful")
+        if(confirm("Confirm the deletion of the row")) {
+            personsList.splice(personsList.findIndex(x => x.id === id), 1);
+            updateLocalStorage();
+            handleFilter();
+            }
     }
+}
+
+function findPerson(id){
+    personsList.forEach(x => {
+        if(x.id === id)
+            return x;
+    })
+    return null;
 }
 
 function mergeRows() {
@@ -152,26 +169,27 @@ function toggleOnEditButtons() {
     document.querySelector('#saveButton').hidden = false;
 }
 
-function handleEdit(rowIndex) {
+function handleEdit(id) {
     if(document.querySelector("#cancelButton").hidden == false) 
         alert("You are already editing a person. Cancel anytime");
     else{
-        document.querySelector(`#firstNameSection .inputField`).value = personsList[rowIndex]['firstName'];
-        document.querySelector(`#lastNameSection .inputField`).value = personsList[rowIndex]['lastName'];
-        document.querySelector(`#ageSection .inputField`).value = personsList[rowIndex]['age'];
+        var personIndex = personsList.findIndex(x => x.id === id);
+        document.querySelector(`#firstNameSection .inputField`).value = personsList[personIndex]['firstName'];
+        document.querySelector(`#lastNameSection .inputField`).value = personsList[personIndex]['lastName'];
+        document.querySelector(`#ageSection .inputField`).value = personsList[personIndex]['age'];
         document.querySelectorAll(`#genderSection .inputField`).forEach(radioButton => {
-            if(radioButton.value == personsList[rowIndex]['gender'])
+            if(radioButton.value == personsList[personIndex]['gender'])
                 radioButton.checked = true;
         });
-        document.querySelector(`#employmentSection .inputField`).checked = personsList[rowIndex]['employed'];
-        document.querySelector(`#studiesSection .inputField`).value = personsList[rowIndex]['study'];
+        document.querySelector(`#employmentSection .inputField`).checked = personsList[personIndex]['employed'];
+        document.querySelector(`#studiesSection .inputField`).value = personsList[personIndex]['study'];
         validateForm();
-        document.querySelector('#saveButton').setAttribute("onclick", `handleSave(${rowIndex})`);
+        document.querySelector('#saveButton').setAttribute("onclick", `handleSave(${id})`);
         toggleOnEditButtons();   
     }
 }
 
-function handleSave(rowIndex) {
+function handleSave(id) {
     validateForm();
     if(formIsValid())
     {
@@ -181,20 +199,21 @@ function handleSave(rowIndex) {
         let gender = getInputValue('genderSection')
         let employed = getInputValue('employmentSection')
         let study = getInputValue('studiesSection');
-        personsList[rowIndex]['firstName'] = firstName;
-        personsList[rowIndex]['lastName'] = lastName;
-        personsList[rowIndex]['age'] = age;
-        personsList[rowIndex]['gender'] = gender;
-        personsList[rowIndex]['employed'] = employed;
-        personsList[rowIndex]['study'] = study;
+        var personIndex = personsList.findIndex(x => x.id === id);
+        personsList[personIndex]['firstName'] = firstName;
+        personsList[personIndex]['lastName'] = lastName;
+        personsList[personIndex]['age'] = age;
+        personsList[personIndex]['gender'] = gender;
+        personsList[personIndex]['employed'] = employed;
+        personsList[personIndex]['study'] = study;
         updateLocalStorage();
-        refreshTable(firstName, lastName, age, gender, employed, study);
+        handleFilter();
         toggleOffEditButtons();
         clearForm();
     }
 }
 
-function sortTableByColumn(c) {
+function sortTableByColumn(c, compare) {
     var table, rows, switching, i, x, y, shouldSwitch;
     table = document.getElementById("personsTable");
     switching = true;
@@ -206,12 +225,12 @@ function sortTableByColumn(c) {
             x = rows[i].getElementsByTagName("TD")[c];
             y = rows[i + 1].getElementsByTagName("TD")[c];
             if(x.innerHTML.match(/^-{0,1}\d+$/) && y.innerHTML.match(/^-{0,1}\d+$/)){
-                if(parseInt(x.innerHTML) > parseInt(y.innerHTML)){
+                if(compare(parseInt(x.innerHTML), parseInt(y.innerHTML))){
                 shouldSwitch = true;
                 break; 
                 }
             }
-            else if(x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+            else if(compare(x.innerHTML.toLowerCase(), y.innerHTML.toLowerCase())) {
                 shouldSwitch = true;
                 break;
             }
@@ -226,8 +245,8 @@ function sortTableByColumn(c) {
 function sortTable(){
     let table = document.getElementById('personsTable');
     let rowLength = table.rows[0].cells.length;
-    for(var i = rowLength - 1 ; i >= 0 ; i--)
-        sortTableByColumn(i);
+    for(var i = rowLength - 1 ; i >= 0 ; i--)0
+        sortTableByColumn(i, (x, y) => { return x - y});
 }
 
 function toggleForm(){
@@ -249,5 +268,28 @@ function toggleShowHideButtons(){
 }
 
 function handleColumnSort(columnIndex){
-    sortTableByColumn(columnIndex);
+    var table = document.getElementById("personsTable");
+    var columns = table.rows[0].cells;
+
+    if(columns[columnIndex].classList.contains("default")) {
+        sortTableByColumn(columnIndex, (x, y) => { return x > y;});
+        columns[columnIndex].classList.add("asc");
+        columns[columnIndex].classList.remove("default");
+    }
+    else if(columns[columnIndex].classList.contains("asc")) {
+        sortTableByColumn(columnIndex, (x, y) => { return x < y;});
+        columns[columnIndex].classList.add("desc");
+        columns[columnIndex].classList.remove("asc");
+    }
+    else {
+        refreshTable(personsList);
+        columns[columnIndex].classList.add("default");
+        columns[columnIndex].classList.remove("desc");
+    }
+}
+
+function handleFilter() {
+    var inputText = document.querySelector("#filterField").value;
+    let filteredList = personsList.filter(x => (x.firstName + x.lastName).toLowerCase().includes(inputText.toLowerCase()));
+    refreshTable(filteredList);
 }
